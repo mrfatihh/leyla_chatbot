a9de91a98bd3e482e1299291426bd5472777527a
mrfatihh <mrfatiharslann@gmail.com>
2025-05-26 21:42:49 +0300

    Add files via upload

---
+++ Messages.py
@@ -0,0 +1,37 @@
+m_start = "⚡️ SELAMAT DATANG ⚡️\n\n🇮🇩 Semoga Dapat teman atau jodoh\n\n🇳🇿 I hope you can make a friend or a partner\n\n💬 untuk mencari teman obrolan gunakan button New Chat"
+
+m_is_not_free_users = (
+    "Sorry you are not in chat"
+)
+
+m_is_connect = "✅ You are already in the chat, please send your chat"
+
+m_play_again = "Do you want to chat with other people?"
+
+m_is_not_user_name = "❌ Sorry, our bot can only communicate if you have a username"
+
+m_good_bye = "❌ Sorry your interlocutor left the chat"
+
+m_disconnect_user = "Sorry, the connection was lost!"
+
+m_failed = "❌ Loss happened"
+
+m_like = "✅ You send the username to the other person"
+
+m_dislike_user = "Chat Has Ended"
+
+m_dislike_user_to = (
+    "⛔ Your interlocutor leaves the chat"
+)
+
+m_send_some_messages = "The bot could not continue the pan from the bot"
+
+m_has_not_dialog = "You are not in chat"
+
+dislike_str = "👎"
+
+like_str = "👍"
+
+
+def m_all_like(x):
+    return "Nih kak " + "username : @" + str(x)
